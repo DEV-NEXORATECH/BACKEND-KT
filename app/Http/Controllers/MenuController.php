1f7fdd9a8443bc13a93c4458b93e5acab8e272d9
git push <raulmahya11@gmail.com>
2026-09-15 14:33:29 +0700

    OK

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -6,7 +6,6 @@ use App\Models\Menu;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-use Illuminate\Validation\Rule;
 
 class MenuController extends Controller
 {
@@ -27,9 +26,8 @@ class MenuController extends Controller
     public function store(Request $request): JsonResponse
     {
         $data = $this->validatedData($request);
-        $slug = ! empty($data['slug']) ? $data['slug'] : $this->uniqueSlug(Str::slug($data['title']));
-        $data['slug'] = $slug;
-        $data['path'] = ! empty($data['path']) ? $data['path'] : '/'.$slug;
+        $data['slug'] = $this->uniqueSlug(Str::slug($data['slug'] ?: $data['title']));
+        $data['path'] = ! empty($data['path']) ? $data['path'] : '/'.$data['slug'];
 
         $menu = Menu::create($data);
 
@@ -42,9 +40,8 @@ class MenuController extends Controller
     public function update(Request $request, Menu $menu): JsonResponse
     {
         $data = $this->validatedData($request, $menu);
-        $slug = ! empty($data['slug']) ? $data['slug'] : $this->uniqueSlug(Str::slug($data['title']), $menu->id);
-        $data['slug'] = $slug;
-        $data['path'] = ! empty($data['path']) ? $data['path'] : '/'.$slug;
+        $data['slug'] = $this->uniqueSlug(Str::slug($data['slug'] ?: $data['title']), $menu->id);
+        $data['path'] = ! empty($data['path']) ? $data['path'] : '/'.$data['slug'];
 
         $menu->update($data);
 
@@ -72,7 +69,6 @@ class MenuController extends Controller
                 'nullable',
                 'string',
                 'max:140',
-                Rule::unique('menus', 'slug')->ignore($menu?->id),
             ],
             'path' => ['nullable', 'string', 'max:180'],
             'icon' => ['nullable', 'string', 'max:60'],
