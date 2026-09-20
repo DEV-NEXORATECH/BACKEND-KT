be81138da08b96b0583722a9fe307742b0e32d6a
git push <raulmahya11@gmail.com>
2026-09-20 20:39:03 +0700

    ok

--- config/cors.php
+++ config/cors.php
@@ -7,12 +7,13 @@ return [
 
     'allowed_origins' => array_filter(array_map('trim', explode(',', env(
         'CORS_ALLOWED_ORIGINS',
-        'http://localhost:5173,http://127.0.0.1:5173,http://localhost:3000,http://127.0.0.1:3000,http://localhost:8080,https://red-lyrebird-398503.hostingersite.com,http://103.103.193.161:90,https://lawngreen-mosquito-967832.hostingersite.com'
+        'http://localhost:5173,http://127.0.0.1:5173,http://localhost:3000,http://127.0.0.1:3000,http://localhost:8080,https://red-lyrebird-398503.hostingersite.com,https://lawngreen-mosquito-967832.hostingersite.com'
     )))),
 
     'allowed_origins_patterns' => [
         '#^http://localhost(:\d+)?$#',
         '#^http://127\.0\.0\.1(:\d+)?$#',
+        '#^https://.*\.hostingersite\.com/?$#',
     ],
 
     'allowed_headers' => ['*'],
